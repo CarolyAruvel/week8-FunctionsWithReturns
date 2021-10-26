0824c3e9e83886db5f1308fa167970c39e7617d4
Opilane TTHK <opilane@tthk.ee>
2021-10-26 13:41:06 +0300

    week8

--- EpicFight/Program.cs
+++ EpicFight/Program.cs
@@ -7,30 +7,48 @@ namespace EpicFight
         static void Main(string[] args)
         {
             string hero, villain;
+            string heroWeapon, villainWeapon;
             hero = RandomHero();
+            heroWeapon = RandomWeapon();
             villain = RandomVillain();
-            Console.WriteLine($"Random hero: {hero}");
+            villainWeapon = RandomWeapon();
+            Console.WriteLine($"{hero} will fight with {villain}");
             Console.WriteLine($"Random villain: {villain}");
+            Console.WriteLine($"{villain} will fight with {villainWeapon}");
+            Console.WriteLine($"{hero} will fight with {heroWeapon}");
+        }
+
+        private static int RandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+
+            return randomIndex;
         }
 
         private static string RandomHero()
         {
             string[] heroes = { "Batman", "Spongebob", "Spiderman", "Patric", "Lara Croft" };
-            Random rnd = new Random();
-            int randomIndex = rnd.Next(0, heroes.Length);
-
-            string randomHero = heroes[randomIndex];
-            return randomHero;
+           
+            return heroes[RandomIndex(heroes)];
 
         }
         private static string RandomVillain()
         {
-        string[] villain = { "Mirjam", "Peppa pig", "Elise" };
+            string[] villain = { "Mirjam", "Peppa pig", "Elise" };
             Random rnd = new Random();
             int randomIndex = rnd.Next(0, villain.Length);
 
             string randomVillain = villain[randomIndex];
             return randomVillain;
+        }
+
+        private static string RandomWeapon()
+        {
+            string[] weapon = { "Gun", "Knife", "Boxing Glove" };
+             return weapon[RandomIndex(weapon)];
+                
+            
         }
 
         
